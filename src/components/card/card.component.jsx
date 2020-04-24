1d818bb4b7f1604987f7e5a9a586f1d90874dd33
Lucas <lucasbezerra1701@gmail.com>
2020-04-23 21:54:04 -0300

    M#03A20

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './card.style.css';
+
+export const Card = ({ monster }) => (
+  <div className="card-container">
+    <img
+      src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+      alt="monster"
+    />
+    <h1>{monster.name}</h1>
+    <p>{monster.email}</p>
+  </div>
+);
