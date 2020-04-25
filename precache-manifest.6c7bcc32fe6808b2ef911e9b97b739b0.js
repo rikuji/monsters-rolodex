63d7fe89fa5eeb38e78edb692f3b9a6ce08c740a
Lucas <lucasbezerra1701@gmail.com>
2020-04-24 21:05:04 -0300

    Updates

---
+++ precache-manifest.6c7bcc32fe6808b2ef911e9b97b739b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a8d246963dc102c6f424d87cd53eb7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "010290d7c63c8426e5d3",
+    "url": "/monsters-rolodex/static/css/main.1611621b.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "010290d7c63c8426e5d3",
+    "url": "/monsters-rolodex/static/js/main.c3e41a3f.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
